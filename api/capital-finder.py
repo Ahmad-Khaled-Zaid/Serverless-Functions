07d666f14847168e921b18f0a329775df90d0ebe
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-07-04 22:32:56 +0300

    solve the lab

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -9,17 +9,17 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
-        capital = []
+        capital = ""
         if 'country' in dic:
-            word = dic['country']
+            country_name = dic['country']
             url = 'https://restcountries.com/v3.1/name/'
-            r = requests.get(url + word)
+            r = requests.get(url + country_name)
             data = r.json()
             for country_data in data:
                 country_capital = country_data['capital'][0]
-                capital.append(country_capital)
+                capital = country_capital
 
-            message = str(capital)
+            message = f"the capital of {country_name} is {capital}"
 
         else:
             message = "Please provide me with a country name"
@@ -30,37 +30,3 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(message.encode())
 
         return
-
-# from http.server import BaseHTTPRequestHandler
-# import requests
-# from urllib import parse
-#
-#
-# class Capital(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         s = self.path
-#         url_components = parse.urlsplit(s)
-#         query_string_list = parse.parse_qsl(url_components.query)
-#         dic = dict(query_string_list)
-#         Capital = ""
-#         if 'country' in dic:
-#             word = dic['country']
-#             url = 'https://restcountries.com/v3.1/name/'
-#             r = requests.get(url + word)
-#             data = r.json()
-#             for capital in data:
-#                 Capital = capital['capital'][0]
-#                 # definitions.append(definition)
-#
-#             # message = str(Capital)
-#             message = "str(Capital)"
-#
-#         else:
-#             message = "Please provide a correct country Name"
-#
-#         self.send_response(200)
-#         self.send_header('Content-type', 'text/plain')
-#         self.end_headers()
-#         self.wfile.write(message.encode())
-#
-#         return
